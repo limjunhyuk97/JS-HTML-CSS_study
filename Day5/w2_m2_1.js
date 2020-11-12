8093850d48a383501d4e7d9b25222152c1243bc3
Jun Hyuk Lim <59442344+limjunhyuk97@users.noreply.github.com>
2020-11-12 12:17:38 +0900

    Update w2_m2_1.js

--- Day5/w2_m2_1.js
+++ Day5/w2_m2_1.js
@@ -17,17 +17,20 @@ class objectAnalyze{
             }
             else if(!isNaN(element)) this.number.push(element);
         });
-        if(braketNum !== 0) console.log('닫는 괄호가 일치하지 않습니다.')
-        else console.log(`깊이 수준은 ${MAX}이며, 총 ${this.number.length}개의 원소가 포함되어 있습니다!`)
+        if(braketNum !== 0) console.log('닫는 괄호가 일치하지 않습니다.');
+        else console.log(`깊이 수준은 ${MAX}이며, 총 ${this.number.length}개의 원소가 포함되어 있습니다!`);
     }
 
-    runInfo(data){
-        let dataArray = this.thinDown(data);
-        dataArray.forEach((element, index) => {
+    runInfo(data, dataType){
+        if(!this.check(data)) return console.log('닫는 괄호가 일치하지 않습니다.');
+        let newData = this.thinDown(data);
+        if(typeof(dataType) === undefined){
+            console.log(`"type" : "root" , "child" : ${[this.runInfo(newData.splice(0,1), newData[1])]}`);
+        }
+        else if(typeof(dataType) === string){
+            console.log(`type : "array" , "child" : ${}`)
+        }
 
-
-
-        })
     }
 
     thinDown(data){
@@ -48,6 +51,22 @@ class objectAnalyze{
             else dataArray.push(splitArray[i]);
         }
         return dataArray;
+    }
+
+    check(data){
+        let braketNum = 0, MAX = 0;
+        let dataArray = this.thinDown(data);
+        dataArray.forEach((element, index) => {
+            if(element === '['){
+                ++braketNum;
+                if(braketNum>MAX) MAX = braketNum;
+            }
+            else if(element == ']'){
+                --braketNum;
+            }
+        });
+        if(braketNum !== 0) return false;
+        else return true;
     }
 
 }

bef91763873fe2ea9f1d60a379d3c01d6035415c
Jun Hyuk Lim <59442344+limjunhyuk97@users.noreply.github.com>
2020-11-12 11:40:16 +0900

    Update w2_m2_1.js

--- Day5/w2_m2_1.js
+++ Day5/w2_m2_1.js
@@ -5,19 +5,22 @@ class objectAnalyze{
 
     run(data){
         this.number.length = 0;
-        let leftBraketNum = 0, rightBraketNum = 0;
+        let braketNum = 0, MAX = 0;
         let dataArray = this.thinDown(data);
         dataArray.forEach((element, index) => {
-            if(element === '[') ++leftBraketNum;
-            else if(element === ']') ++rightBraketNum;
+            if(element === '['){
+                ++braketNum;
+                if(braketNum>MAX) MAX = braketNum;
+            }
+            else if(element == ']'){
+                --braketNum;
+            }
             else if(!isNaN(element)) this.number.push(element);
         });
-        console.log(`원소는 ${this.number} 들이 존재합니다.`)
-        if(leftBraketNum !== rightBraketNum) console.log('닫는 괄호가 일치하지 않습니다.')
-        else console.log(`깊이 수준은 ${leftBraketNum}이며, 총 ${this.number.length}개의 원소가 포함되어 있습니다!`)
+        if(braketNum !== 0) console.log('닫는 괄호가 일치하지 않습니다.')
+        else console.log(`깊이 수준은 ${MAX}이며, 총 ${this.number.length}개의 원소가 포함되어 있습니다!`)
     }
-    
-    // 3번 문제 source code 들어갈 예정입니다.
+
     runInfo(data){
         let dataArray = this.thinDown(data);
         dataArray.forEach((element, index) => {
@@ -41,7 +44,7 @@ class objectAnalyze{
                 dataArray.push(splitArray.slice(tmp, i).join(''));
                 dataArray.push(splitArray[i]);
             }
-            else if(!isNaN(splitArray[i]) && flag === 0) continue;
+            else if(!isNaN(splitArray[i]) && flag === 1) continue;
             else dataArray.push(splitArray[i]);
         }
         return dataArray;

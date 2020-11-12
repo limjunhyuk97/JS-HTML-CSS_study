97c0f95a2e438065dfdfdd9187f6220e73cca9f4
Jun Hyuk Lim <59442344+limjunhyuk97@users.noreply.github.com>
2020-11-12 18:51:15 +0900

    Update w2_m2_1.js

--- Day5/w2_m2_1.js
+++ Day5/w2_m2_1.js
@@ -20,18 +20,10 @@ class objectAnalyze{
         if(braketNum !== 0) console.log('닫는 괄호가 일치하지 않습니다.');
         else console.log(`깊이 수준은 ${MAX}이며, 총 ${this.number.length}개의 원소가 포함되어 있습니다!`);
     }
-    
-    // 3번 문제 
-    runInfo(data, dataType){
-        if(!this.check(data)) return console.log('닫는 괄호가 일치하지 않습니다.');
-        let newData = this.thinDown(data);
-        if(typeof(dataType) === undefined){
-            console.log(`"type" : "root" , "child" : ${[this.runInfo(newData.splice(0,1), newData[1])]}`);
-        }
-        else if(typeof(dataType) === string){
-            console.log(`type : "array" , "child" : ${}`)
-        }
 
+    runInfo(data){
+        let dataArray = this.thinDown(data);
+        
     }
 
     thinDown(data){
@@ -46,28 +38,14 @@ class objectAnalyze{
             else if(isNaN(splitArray[i]) && flag ===1){
                 flag = 0;
                 dataArray.push(splitArray.slice(tmp, i).join(''));
-                dataArray.push(splitArray[i]);
+                if(splitArray[i] !== ',') dataArray.push(splitArray[i]);
             }
             else if(!isNaN(splitArray[i]) && flag === 1) continue;
-            else dataArray.push(splitArray[i]);
+            else{
+                if(splitArray[i] !== ',') dataArray.push(splitArray[i]);
+            }
         }
         return dataArray;
-    }
-
-    check(data){
-        let braketNum = 0, MAX = 0;
-        let dataArray = this.thinDown(data);
-        dataArray.forEach((element, index) => {
-            if(element === '['){
-                ++braketNum;
-                if(braketNum>MAX) MAX = braketNum;
-            }
-            else if(element == ']'){
-                --braketNum;
-            }
-        });
-        if(braketNum !== 0) return false;
-        else return true;
     }
 
 }
